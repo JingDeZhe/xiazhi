66de50a1061bfd137938a26129d462cfb32daeaa
lotusloli <nymphaeal@qq.com>
2023-05-25 11:18:03 +0800

    1. public目录修复；

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -4,7 +4,14 @@ const { defineConfig } = require('vitepress')
 const config = defineConfig({
   base: '/xiazhi/',
   outDir: '../dist',
+  lang: 'zh-CN',
   title: '夏至',
+  head: [
+    [
+      'link',
+      { rel: 'icon', type: 'image/x-icon', href: '/xiazhi/favicon.ico' },
+    ],
+  ],
   themeConfig: {
     sidebar: generateSidebar(),
   },
