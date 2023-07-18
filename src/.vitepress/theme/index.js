81beef2a6e6af40c48265360d51bb6c91debd5ca
lotusloli <nymphaeal@qq.com>
2023-07-18 14:58:04 +0800

    1. 更新依赖，修改全局字体；

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import './custom.css'
 
 export default DefaultTheme
