406e4ee635990119c200f0652f792956d08eb97c
lotus <1504558766@qq.com>
2024-05-08 19:14:53 +0800

    1. t;

--- src/docs/02.3-应用/10-碎片拼图/assets/FragmentPuzzle01.vue
+++ src/docs/02.3-应用/10-碎片拼图/assets/FragmentPuzzle01.vue
@@ -3,14 +3,27 @@ import { ref } from 'vue'
 
 const fragments = ref([])
 
-const tmpFragment = ref([])
+const tmpFragmentPoints = ref([])
 const activeFragment = ref(null)
+
+const getPoints = (fragment) => {
+  return fragment.points
+}
 </script>
 
 <template>
-  <div class="fragment-puzzle">
-    <svg width="300px" height="300px" viewBox="0 0 100 100"></svg>
+  <div class="fragment-puzzle-01">
+    <svg width="300px" height="300px" viewBox="0 0 100 100">
+      <polygon v-for="p in fragments" :points="getPoints(p)"></polygon>
+    </svg>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.fragment-puzzle-01 {
+  background-color: #eee;
+  aspect-ratio: 1/1;
+  border-radius: 3px;
+  overflow: hidden;
+}
+</style>
