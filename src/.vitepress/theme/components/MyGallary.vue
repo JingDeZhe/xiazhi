2c3f3b61531cc6377e2b26079afb58417590ad5b
lotusloli <nymphaeal@qq.com>
2024-05-15 11:29:56 +0800

    1. 记录

---
+++ src/.vitepress/theme/components/MyGallary.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import GLightbox from 'glightbox'
+
+const ctn = ref()
+const props = defineProps({
+  imgs: { type: Array, default: () => [] },
+})
+
+let gallary
+onMounted(() => {
+  gallary = new GLightbox({
+    selector: ctn.value,
+    elements: imgs,
+  })
+})
+</script>
+
+<template>
+  <div ref="ctn"></div>
+</template>
+
+<style lang="scss"></style>
--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -3,11 +3,13 @@ import './custom.css'
 import 'sweetalert2/src/sweetalert2.scss'
 import MyIframe from './components/MyIframe.vue'
 import MyA from './components/MyA.vue'
+import MyGallary from './components/MyGallary.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('my-iframe', MyIframe)
     app.component('my-a', MyA)
+    app.component('my-gallary', MyGallary)
   },
 }
