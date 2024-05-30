3f5219d5e06a422d18dc4da3714d44f157604925
lotusloli <nymphaeal@qq.com>
2024-05-30 23:09:14 +0800

    1. avatar

---
+++ src/docs/01-记录/60-参考/assets/AvatarGallary.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const index = ref('1')
+const src = computed(() => {
+  const t = index.value.toString().padStart(3, '0')
+  return `https://store-1258290249.cos.ap-guangzhou.myqcloud.com/image/avatar/avatar-${t}.png`
+})
+</script>
+
+<template>
+  <div>
+    <input type="number" v-model="index" />
+    <img :src="src" alt="avatar" />
+  </div>
+</template>
+
+<style lang="scss"></style>
